765d14a300c61edcc884fa4177d3a6d74b293ca2
Daphney <daphneyalexandre@hotmail.com>
2024-05-09 19:35:11 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+// const element = document.createElement("div");
+// document.body.append(element);
+
+
+const main = document.querySelector('main')
+main.remove();
+
+
+const newHeader = document.createElement("h1")
+
+ newHeader.id = "victory"
+
+ newHeader.innerHTML = "Daphney is the champion"
